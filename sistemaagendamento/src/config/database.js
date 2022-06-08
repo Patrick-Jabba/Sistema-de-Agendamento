f8af44f76eccf53c2dd9abbd1b9568eb50853010
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-06-08 14:31:46 -0300

    update properties

--- sistemaagendamento/src/config/database.js
+++ sistemaagendamento/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'iohtk67DROME',
+    password: 'insirasuasenha',
     database: 'sistema',
     define: {
         timestamps: true,
